ca3344292fe1b5703acabd32dff3a9691da1df36
Roushan <roushanp2003@gmail.com>
2025-05-21 20:52:02 +0530

    Fix query in GET endpoint to retrieve searches by email instead of sid; update notebook with corrected price and amenities data

--- client_side/app/api/search/getById/[sid]/route.js
+++ client_side/app/api/search/getById/[sid]/route.js
@@ -4,7 +4,7 @@ export const GET = async (req, { params }) => {
   const { sid } = await params;
   console.log(sid);
   const db = await ConnectDB();
-  const query = `select * from searches where sid = '${sid}'`;
+  const query = `select * from searches where email = '${sid}'`;
   const [res] = await db.query(query);
   return Response.json(res);
 };
